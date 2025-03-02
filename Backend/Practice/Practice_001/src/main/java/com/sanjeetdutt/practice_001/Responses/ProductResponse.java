6f4c2a91e405aa07df6337d7f6dfbeb31b5b425b
Sanjeet Dutt <sanjeetdutt@gmail.com>
2025-03-02 17:01:36 +0530

    Changing response of first controller

---
+++ Backend/Practice/Practice_001/src/main/java/com/sanjeetdutt/practice_001/Responses/ProductResponse.java
@@ -0,0 +1,58 @@
+package com.sanjeetdutt.practice_001.Responses;
+
+import com.sanjeetdutt.practice_001.models.Status;
+
+public class ProductResponse {
+    String name;
+    String description;
+    String imageUrl;
+    Integer price;
+    String status;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        switch (status){
+            case ACTIVE :
+                this.status = "ACTIVE";
+                break;
+
+            default:
+                this.status = "DELETED";
+        }
+    }
+}
--- Backend/Practice/Practice_001/src/main/java/com/sanjeetdutt/practice_001/controllers/ProductController.java
+++ Backend/Practice/Practice_001/src/main/java/com/sanjeetdutt/practice_001/controllers/ProductController.java
@@ -1,6 +1,8 @@
 package com.sanjeetdutt.practice_001.controllers;
 
+import com.sanjeetdutt.practice_001.Responses.ProductResponse;
 import com.sanjeetdutt.practice_001.models.Product;
+import com.sanjeetdutt.practice_001.models.Status;
 import com.sanjeetdutt.practice_001.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -18,12 +21,38 @@ public class ProductController {
     ProductService productService;
 
     @GetMapping("/{id}")
-    public Product GetProductById(@PathVariable Long id){
-        return productService.getProductById(id);
+    public ProductResponse GetProductById(@PathVariable Long id){
+        return this.GetProductResponse(productService.getProductById(id));
     }
 
     @GetMapping("/category/{categoryId}")
-    public List<Product> GetProductByCategory(@PathVariable Long categoryId){
-        return productService.getProductsByCategory(categoryId);
+    public List<ProductResponse> GetProductByCategory(@PathVariable Long categoryId){
+        /*List<ProductResponse> productResponseList = new ArrayList<>();
+
+        for(Product product : productService.getProductsByCategory(categoryId)){
+            productResponseList.add(
+                    GetProductResponse(product)
+            );
+        }
+
+        return productResponseList;*/
+
+        return productService.getProductsByCategory(categoryId)
+                .stream()
+                .filter(product -> product.getStatus() == Status.ACTIVE)
+                .map(this::GetProductResponse)
+                .toList();
+    }
+
+    private ProductResponse GetProductResponse(Product product){
+        ProductResponse productResponse = new ProductResponse();
+
+        productResponse.setDescription(product.getDescription());
+        productResponse.setName(product.getName());
+        productResponse.setImageUrl(product.getImageUrl());
+        productResponse.setPrice(product.getPrice());
+        productResponse.setStatus(product.getStatus());
+
+        return productResponse;
     }
 }
