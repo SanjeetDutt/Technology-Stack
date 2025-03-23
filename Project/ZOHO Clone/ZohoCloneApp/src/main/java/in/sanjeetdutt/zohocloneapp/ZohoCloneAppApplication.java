b01c45c07b4f34e78956f4b1b6e2d25a7816270d
Sanjeet Dutt <sanjeetdutt@gmail.com>
2025-03-23 17:18:09 +0530

    ZOHO Clone project setup

---
+++ Project/ZOHO Clone/ZohoCloneApp/src/main/java/in/sanjeetdutt/zohocloneapp/ZohoCloneAppApplication.java
@@ -0,0 +1,13 @@
+package in.sanjeetdutt.zohocloneapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZohoCloneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZohoCloneAppApplication.class, args);
+	}
+
+}
