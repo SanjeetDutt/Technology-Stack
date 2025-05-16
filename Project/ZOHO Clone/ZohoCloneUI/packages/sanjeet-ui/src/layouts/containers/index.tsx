445d645273f971964293330cbbcd43b85b99c9da
Sanjeet Dutt <sanjeetdutt@gmail.com>
2025-05-16 22:10:46 +0530

    Adding pacakge

---
+++ Project/ZOHO Clone/ZohoCloneUI/packages/sanjeet-ui/src/layouts/containers/index.tsx
@@ -0,0 +1,5 @@
+export const Containers = ()=>{
+
+
+    return <p>CONTAINER COMPONENT</p>
+}
---
+++ Project/ZOHO Clone/ZohoCloneUI/packages/sanjeet-ui/src/main.ts
@@ -0,0 +1,5 @@
+// @ts-ignore
+import * as styles from "./index.scss"
+
+export * from "./layouts"
+export * from "./components"
